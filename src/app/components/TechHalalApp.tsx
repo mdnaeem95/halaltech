3d83b5f2a27b0d1e0c56f5c508c954acc18583ea
Naeem Sani <muhdnaeem95@gmail.com>
2025-06-09 20:02:10 +0800

    Added use client on component.

--- src/app/components/TechHalalApp.tsx
+++ src/app/components/TechHalalApp.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react';
 import { Menu, X, Code, Smartphone, Monitor, Palette, ChevronRight, Star, Phone, Mail, MapPin, Check, ArrowRight } from 'lucide-react';
 
